eef9c22f1ae5176284f476d565ebf5225dbccaa1
Ankitshah89 <shahdankit@gmail.com>
2020-01-26 21:40:18 -0700

    Weekend Project

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 
 
-## Make Change Project
+# Make Change Project
 
-### Overview
-**This is a cash register application.**  _italicized_  *application*
-It asks the user for the amount of an item.
+## Overview
+
+This is a cash register application.
+It asks the user for the amount of an item and returns the corresponding change. It also checks if the amount paid is not enough. 
+If the amount paid is equal to the amount owed, there is a thank you message.
+
 
 ## Technologies Used
 
 * Java
 * Eclipse
+* Git
 
 
 ## Lessons Learned
+* I learned the use and importance of rounding numbers.
+* I learned how to use MOD operators.
+
+
+ _Overall, this was a fun weekend project to work on. As we learn more about method in the coming days, I will definitely come back to this problem and try to update it._
 
--- src/com/skillditillery/makechange/MakeChange.java
+++ src/com/skillditillery/makechange/MakeChange.java
@@ -1,6 +1,7 @@
 package com.skillditillery.makechange;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class MakeChange {
 
@@ -9,14 +10,75 @@ public class MakeChange {
 		Scanner kb = new Scanner(System.in);
 		System.out.println("*************** \tCASH REGISTER\t ****************");
 		System.out.print("Enter the price of the item: ");
-		double price = kb.nextDouble();
-		askItemPrice(price);
+		double itemPrice = kb.nextDouble();
+		System.out.print("Enter the amount tendered: ");
+		double tenderedAmount = kb.nextDouble();
+		System.out.println(totalChange(itemPrice, tenderedAmount));
+
+		DecimalFormat df = new DecimalFormat("0.00");
 
 		kb.close();
 	}
 
-	public static double askItemPrice(double price) {
-		return price;
-	}
+	public static String totalChange(double totalPrice, double totalPayment) {
+		DecimalFormat df = new DecimalFormat("0.00");
 
+		String billsNeeded = "";
+
+		double change = (totalPayment * 100 - totalPrice * 100);
+		if (change == 0) {
+			System.out.println("Thank you for shopping with us!");
+		} else if (change < 0) {
+			System.out.println("Sorry! You are $" + df.format(totalPrice - totalPayment) + " short.");
+
+		}
+		while (change > 0) {
+			if (change >= 2000) {
+				billsNeeded += ("\n$20 : " + ((int) change / 2000));
+				change %= 2000;
+
+			} else if (change >= 1000) {
+				billsNeeded += ("\n$10 : " + ((int) change / 1000));
+				change %= 1000;
+
+			}
+
+			else if (change >= 500) {
+				billsNeeded += ("\n$5 : " + ((int) change / 500));
+				change %= 500;
+
+			}
+
+			else if (change >= 100) {
+				billsNeeded += ("\n$1 : " + ((int) change / 100));
+				change %= 100;
+
+			}
+
+			else if (change >= 25) {
+				billsNeeded += ("\n$0.25 : " + ((int) change / 25));
+				change %= 25;
+
+			}
+
+			else if (change >= 10) {
+				billsNeeded += ("\n$0.10 : " + ((int) change / 10));
+				change %= 10;
+
+			}
+
+			else if (change >= 5) {
+				billsNeeded += ("\n$0.05 : " + ((int) change / 5));
+				change %= 5;
+
+			} else if (change >= 1) {
+				billsNeeded += ("\n$0.01 : " + ((int) change / 1));
+				change %= 1;
+
+			}
+
+		}
+		return billsNeeded;
+
+	}
 }
